c3f6d7a7d62004501271812c9e6b7782fd2b4178
Muhammad Tahir Sajjad <108147112+notreallyTahir@users.noreply.github.com>
2026-09-12 23:17:12 +0200

    Update signal generation with JSON config and wave types
    Refactor generate_input_signal to use configuration from JSON file and support multiple wave types.

--- signal_gen.py
+++ signal_gen.py
@@ -1,11 +1,49 @@
 import numpy as np
-def generate_input_signal(signal_freq=1000, noise_freq=25000, fs= 100000, duration=0.01):
-    num_samples = int(fs*duration) # uniform samples, samples/time * time = samples
-    Tarray = np.linspace(0, duration, num_samples)
+import json
+from scipy import signal
+
+
+# json file to save data
+with open('config.json', 'r') as file:
+    values = json.load(file)
+
+
+def generate_input_signal(cfg=values):
+    signal_freq = cfg['Signal Frequency']
+    noise_freq = cfg['Noise Frequency']
+    Cycles = cfg['Number of Cycles']
+    fs = cfg['Sampling Frequency']
+    wave = cfg['Signal Type']
+    amp = cfg['Signal Amplitude']
+
+
+    Period = 1 / signal_freq
+    duration = Cycles*Period
+    samples = int(fs * duration)
+    Tarray = np.linspace(0, duration, samples)
+    phase = 2*np.pi*signal_freq*Tarray
+
 
     # Voltage formula = V = A sin(wt), where w = 2pi * f
-    V_signal = 2 * np.sin(2*np.pi*signal_freq*Tarray) 
-    V_Noise = 0.5 * np.sin(2*np.pi*noise_freq*Tarray)
+    #noise amplitude 0.5
+
+    if wave == 'sin':
+        # Voltage formula = V = A sin(wt), where w = 2pi * f
+        V_signal = amp * np.sin(phase) 
+        V_Noise = 0.5 * np.sin(2*np.pi*noise_freq*Tarray)
+
+    elif wave == 'square':
+        V_signal = amp*signal.square(phase)
+        V_Noise = 0.5 * np.sin(2*np.pi*noise_freq*Tarray)
+
+    elif wave == 'triangle':
+        V_signal = amp*signal.sawtooth(phase, width=0.5)
+        V_Noise = 0.5 * np.sin(2*np.pi*noise_freq*Tarray)
+
+    else:
+        raise ValueError(f'Unknown signal type {wave}')
 
     Vin = V_signal + V_Noise
-    return Vin, Tarray
+
+        
+    return Vin, V_signal, V_Noise, Tarray, cfg
